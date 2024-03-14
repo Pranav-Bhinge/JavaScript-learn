b327bfd84e7b393a16e768fc4f218714aabd743b
Pranav-Bhinge <136009215+Pranav-Bhinge@users.noreply.github.com>
2024-03-14 18:07:23 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3 + 3)//we are using nodejs,not browser
+let age=18
+let name="Pranav"
+let isliggedIn=false
+let state=null
+let city;
+
+
+
+//number =>2 to power 53
+//bigint => more big integer values
+//string => "dsgf"
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof undefined)//datatype is undefined only
+console.log(typeof null) //datatype is object
