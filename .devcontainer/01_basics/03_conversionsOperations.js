58623b72409afcf024141c8376953be5b8bb4915
Pranav-Bhinge <136009215+Pranav-Bhinge@users.noreply.github.com>
2024-03-16 13:21:53 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionsOperations.js
@@ -0,0 +1,27 @@
+let score="33anc"//if only number then it will convert it 
+                    /*but if it has num and string at a time
+                    then also it conver but the real type is 
+                    NaN(not a number)
+                    NaN will be printed in case of null,String and undefined*/
+
+console.log(typeof score);
+let valueInNum=Number(score)
+console.log(typeof valueInNum);
+console.log( valueInNum);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true =>1 ; false => 0
+
+let isloggedIn=1
+let booleanIsLoggedIn=Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true 0 => false
+//"" => false
+//"hitesh" => true
+
+let somenumber=33
+let somenumberString=String(somenumber);
+console.log(typeof somenumberString);
