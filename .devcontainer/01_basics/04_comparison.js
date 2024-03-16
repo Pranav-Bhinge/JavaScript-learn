416cfb0110a84fcbb224917084c5fdfd4dbd7f7a
Pranav-Bhinge <136009215+Pranav-Bhinge@users.noreply.github.com>
2024-03-16 19:10:39 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+//console.log(2>1);
+//console.log(2<1);
+//console.log(2>=1);
+//console.log(2==1);
+//console.log(2!=1);
+//console.log("2">1);//do not do this it will not give predictable results
+//console.log("02">1);
+
+console.log("2"===2);//strict comparison it will check value
+//                      and datatype both so will produce false output
