82d94ec337843b29c16edf267cd6b33ee4906925
Pranav-Bhinge <136009215+Pranav-Bhinge@users.noreply.github.com>
2024-03-18 08:10:38 +0000

    Strings

---
+++ .devcontainer/01_basics/05_Strings.js
@@ -0,0 +1,21 @@
+const name="Pranav";
+const repoCount=50
+
+//console.log(name+repoCount+"value"); dont use this outdated
+
+console.log(`hello my name is ${name.Totuppercase} and ny repo count is ${repoCount}`);
+//use backteks`` (String interpolation) these are placeholder$
+
+
+const gamename=new String('Pranav')
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt("5"));
+console.log(gamename.indexOf("a")); 
+
+const newstring=gamename.substring(0,4);//slicing
+console.log(newstring);
